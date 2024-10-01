37af64d28e1d59c1a25d8fdf9dd6368f9e0702ba
Minhwa Cho <78397451+MinhwaCho@users.noreply.github.com>
2024-10-02 02:09:10 +0900

    Title: 무~, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 241002/무~/moo.py
+++ 241002/무~/moo.py
@@ -1,17 +1,41 @@
-N=int(input())
+def find_nth_character(N):
+    def length_of_s(k):
+        # S(k)의 길이를 계산
+        if k == 0:
+            return 3  # S(0) = "moo"의 길이
+        return 2 * length_of_s(k - 1) + (k + 3)
 
-# s = [["m","o","o"]]
-# def divide_conquer(ind):
-#     return s[ind-1] + ["m"] + ["o"]*(ind+2) + s[ind-1]
-# for i in range(1, N+1):
-#     s.append(divide_conquer(i))
-# print(s[N][N-1])
+    def solve(k, n):
+        if k == 0:
+            # S(0) = "moo"
+            if n == 1:
+                return 'm'
+            else:
+                return 'o'
+        
+        left_len = length_of_s(k - 1)  # S(k-1)의 길이
+        middle_len = k + 3  # 가운데 "moo...o" 부분의 길이
+        
+        if n <= left_len:
+            # 왼쪽 S(k-1) 부분에 속할 때
+            return solve(k - 1, n)
+        elif n <= left_len + middle_len:
+            # 가운데 "m o o...o" 부분에 속할 때
+            if n == left_len + 1:
+                return 'm'
+            else:
+                return 'o'
+        else:
+            # 오른쪽 S(k-1) 부분에 속할 때
+            return solve(k - 1, n - left_len - middle_len)
 
-def divide_conquer(x):
-    # if x == N: return
-    if x == 0:
-        return ["m","o","o"]
-    else:
-        el = divide_conquer(x-1)
-        return el + ["m"] + ["o"]*(x+2) + el
-print(divide_conquer(N)[N-1])
+    # k는 N번째 문자가 존재하는 S(t)에서 t를 찾아야 함
+    k = 0
+    while length_of_s(k) < N:
+        k += 1
+    
+    return solve(k, N)
+
+# 입력받은 N번째 문자 출력
+N = int(input())
+print(find_nth_character(N))
