90e95bfe3be85150c36863a1f0b908d99d0cf2a0
Minhwa Cho <78397451+MinhwaCho@users.noreply.github.com>
2024-10-02 02:05:09 +0900

    Title: 무~, Time: 1074ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 241002/무~/moo.py
+++ 241002/무~/moo.py
@@ -1,8 +1,17 @@
 N=int(input())
 
-s = [["m","o","o"]]
-def divide_conquer(ind):
-    return s[ind-1] + ["m"] + ["o"]*(ind+2) + s[ind-1]
-for i in range(1, N+1):
-    s.append(divide_conquer(i))
-print(s[N][N-1])
+# s = [["m","o","o"]]
+# def divide_conquer(ind):
+#     return s[ind-1] + ["m"] + ["o"]*(ind+2) + s[ind-1]
+# for i in range(1, N+1):
+#     s.append(divide_conquer(i))
+# print(s[N][N-1])
+
+def divide_conquer(x):
+    # if x == N: return
+    if x == 0:
+        return ["m","o","o"]
+    else:
+        el = divide_conquer(x-1)
+        return el + ["m"] + ["o"]*(x+2) + el
+print(divide_conquer(N)[N-1])
