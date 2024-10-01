62b6735337b1f8bc99005e2bb3db224dec5a18df
Minhwa Cho <78397451+MinhwaCho@users.noreply.github.com>
2024-10-02 01:55:57 +0900

    Title: 무~, Time: 127ms, Memory: 35MB, Status: Wrong Answer - CodeTree

---
+++ 241002/무~/moo.py
@@ -0,0 +1,8 @@
+N=15
+
+s = [["m","o","o"]]
+def divide_conquer(ind):
+    return s[ind-1] + ["m"] + ["o"]*(ind+2) + s[ind-1]
+for i in range(1, N+1):
+    s.append(divide_conquer(i))
+print(s[N][N-1])
