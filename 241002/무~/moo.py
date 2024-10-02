9cc4d9591aaa98262c7f56baa61a8c7dc1a8b1ea
Minhwa Cho <78397451+MinhwaCho@users.noreply.github.com>
2024-10-02 16:56:58 +0900

    Title: 무~, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 241002/무~/moo.py
+++ 241002/무~/moo.py
@@ -1,41 +1,32 @@
-def find_nth_character(N):
-    def length_of_s(k):
-        # S(k)의 길이를 계산
-        if k == 0:
-            return 3  # S(0) = "moo"의 길이
-        return 2 * length_of_s(k - 1) + (k + 3)
+import sys
+input = sys.stdin.readline
 
-    def solve(k, n):
-        if k == 0:
-            # S(0) = "moo"
-            if n == 1:
-                return 'm'
-            else:
-                return 'o'
-        
-        left_len = length_of_s(k - 1)  # S(k-1)의 길이
-        middle_len = k + 3  # 가운데 "moo...o" 부분의 길이
-        
-        if n <= left_len:
-            # 왼쪽 S(k-1) 부분에 속할 때
-            return solve(k - 1, n)
-        elif n <= left_len + middle_len:
-            # 가운데 "m o o...o" 부분에 속할 때
-            if n == left_len + 1:
-                return 'm'
-            else:
-                return 'o'
-        else:
-            # 오른쪽 S(k-1) 부분에 속할 때
-            return solve(k - 1, n - left_len - middle_len)
-
-    # k는 N번째 문자가 존재하는 S(t)에서 t를 찾아야 함
-    k = 0
-    while length_of_s(k) < N:
-        k += 1
-    
-    return solve(k, N)
-
-# 입력받은 N번째 문자 출력
 N = int(input())
-print(find_nth_character(N))
+
+def cal_length(k):
+    if k == 0: return 3
+    return cal_length(k-1)*2 + (k+3)
+
+def d_c(k, n):
+    if k == 0:
+        if n == 1: return "m"
+        else: return "o"
+
+    else:
+        left_length = cal_length(k-1)
+        middle_length = k+3
+        if n <= left_length:
+            return d_c(k-1, n)
+        elif n > (left_length + middle_length):
+            return d_c(k-1, n - left_length - middle_length)
+        else: # 중앙
+            if n == left_length + 1: return "m"
+            else: return "o"
+
+# k가 포함되어 있는 S(k) 구하기
+k = 0
+while cal_length(k) < N:
+    k += 1
+
+res = d_c(k, N)
+print(res)
