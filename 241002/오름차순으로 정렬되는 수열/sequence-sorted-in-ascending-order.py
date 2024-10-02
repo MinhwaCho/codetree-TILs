db0adbc00269482d42115c740164b941f5e4b4a8
Minhwa Cho <78397451+MinhwaCho@users.noreply.github.com>
2024-10-02 19:01:05 +0900

    Title: 오름차순으로 정렬되는 수열, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241002/오름차순으로 정렬되는 수열/sequence-sorted-in-ascending-order.py
+++ 241002/오름차순으로 정렬되는 수열/sequence-sorted-in-ascending-order.py
@@ -5,19 +5,29 @@ check_list = [int(input()) for i in range(N)]
 
 target_ind = 0
 for i in range(N-1):
-    if check_list[i] > check_list[i+1]: 
-        target_ind = i+1
+    if check_list[i] > check_list[i+1]:
+        if i!=N-2 and check_list[i] > check_list[i-1]:
+            target_ind = i
+            arr = check_list[target_ind+1:]
+        else:
+            target_ind = i+1
+            arr = check_list[:target_ind]
 
 # print("target index: ", target_ind)
-arr = check_list[:target_ind]
 # print("Array:", arr)
 
 cnt = 0
 target_num = check_list[target_ind]
-for i in range(len(arr)-1,-1,-1):
-    if arr[i] > target_num:
-        if i >= 1 and arr[i] == arr[i-1]: 
-            continue
-        else:
+if target_ind < N-1 and target_num > check_list[target_ind+1]:
+    for i in range(len(arr)):
+        if arr[i] < target_num:
+            if i < len(arr)-1 and arr[i] == arr[i+1]: continue
             cnt += 1
+else:
+    for i in range(len(arr)-1,-1,-1):
+        if arr[i] > target_num:
+            if i >= 1 and arr[i] == arr[i-1]: 
+                continue
+            else:
+                cnt += 1
 print(cnt)
